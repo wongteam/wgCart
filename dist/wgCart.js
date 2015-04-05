a35938d6c3bdbe216e34addfd4023c876e888b73
wongteam <team.iwong@gmail.com>
2015-04-05 16:47:33 +0300

    RELEASE 0.1

--- dist/wgCart.js
+++ dist/wgCart.js
@@ -338,6 +338,94 @@ angular.module('wgCart', ['wgCart.directives'])
 
     .value('version', '0.0.3-rc.1');
 
+'use strict';
+
+
+angular.module('wgCart.directives', ['wgCart.fulfilment'])
+
+    .controller('CartController',['$scope', 'wgCart', function($scope, wgCart) {
+        $scope.wgCart = wgCart;
+    }])
+
+    .directive('wgcartAddtocart', ['wgCart', function(wgCart){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {
+                id:'@',
+                name:'@',
+                quantity:'@',
+                quantityMax:'@',
+                price:'@',
+                data:'='
+            },
+            transclude: true,
+            templateUrl: 'template/wgCart/addtocart.html',
+            link:function(scope, element, attrs){
+                scope.attrs = attrs;
+                scope.inCart = function(){
+                    return  wgCart.getItemById(attrs.id);
+                };
+
+                if (scope.inCart()){
+                    scope.q = wgCart.getItemById(attrs.id).getQuantity();
+                } else {
+                    scope.q = parseInt(scope.quantity);
+                }
+
+                scope.qtyOpt =  [];
+                for (var i = 1; i <= scope.quantityMax; i++) {
+                    scope.qtyOpt.push(i);
+                }
+
+            }
+
+        };
+    }])
+
+    .directive('wgcartCart', [function(){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {},
+            templateUrl: 'template/wgCart/cart.html',
+            link:function(scope, element, attrs){
+
+            }
+        };
+    }])
+
+    .directive('wgcartSummary', [function(){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {},
+            transclude: true,
+            templateUrl: 'template/wgCart/summary.html'
+        };
+    }])
+
+    .directive('wgcartCheckout', [function(){
+        return {
+            restrict : 'E',
+            controller : ('CartController', ['$scope', 'wgCart', 'fulfilmentProvider', function($scope, wgCart, fulfilmentProvider) {
+                $scope.wgCart = wgCart;
+
+                $scope.checkout = function () {
+                    fulfilmentProvider.setService($scope.service);
+                    fulfilmentProvider.setSettings($scope.settings);
+                    var promise = fulfilmentProvider.checkout();
+                    console.log(promise);
+                }
+            }]),
+            scope: {
+                service:'@',
+                settings:'='
+            },
+            transclude: true,
+            templateUrl: 'template/wgCart/checkout.html'
+        };
+    }]);
 
 angular.module('wgCart.fulfilment', [])
     .service('fulfilmentProvider', ['$injector', function($injector){
